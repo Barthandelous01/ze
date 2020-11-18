b30d5a3dac796fb132f5a9746f3650402d84ee1f
Barthandelous01 <calvarium2009@hotmail.com>
2020-11-18 17:34:14 -0500

    interface: add longopts and boilerplate
    Add some boilerplate for longopts. Also do some fun comparisons so
    that non-arg arguments (e.g. `ze [id]`) don't break longopt.

--- src/main.c
+++ src/main.c
@@ -1,8 +1,44 @@
 #include <stdio.h>
+#include <getopt.h>
+#include <stdlib.h>
 #include <lmdb.h>
 
-int main (void)
+#include <config.h>
+
+static void version()
 {
-	printf ("%s\n", "Hello, world!\n");
+	printf("%s", PACKAGE_STRING "\nCopyright (C) 2020 Barthandelous01\n\
+License RBSD 3-Clause License.\n\
+This is free software; you are free to change and redistribute it.\n\
+There is NO WARRANTY, to the extent permitted by law.");
+	exit(0);
+}
+
+int main (int argc, char **argv)
+{
+	static int quiet = 0;
+	static struct option longopts[] = {
+		{"version",    no_argument,       NULL, 'V'},
+		{"help",       no_argument,       NULL, 'h'},
+		{"new",        no_argument,       NULL, 'n'},
+		{"edit",       required_argument, NULL, 'e'},
+		{"remove",     required_argument, NULL, 'r'}
+	};
+	int ch = 0;
+	while ((ch = getopt_long(argc, argv, "Vhne:r:", longopts, NULL)) != -1)
+	{
+		quiet = 1;
+		switch(ch) {
+		case 'V':
+			version();
+			break;
+		}
+	}
+
+	if ((quiet == 0) && (argc >= 2))
+		printf("Got single arg: %s\n", argv[1]);
+	else
+		printf("Do default thing\n");
+
 	return 0;
 }

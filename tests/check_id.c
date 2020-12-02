cae4bea998676e038dfe47a29a3e621f5c633e83
Caleb Brzezinski <calvarium2009@hotmail.com>
2020-12-02 14:43:45 -0500

    tests: improve documentation
    Add documentation to test functions and establish naming pattern for
    future tests.
    
    Tests have to be cross-platform and portable. However, many of the
    functions in ``ze'' use files and similar structures to inform their
    behavior. Therefore, for any file that is used in a test that is
    passed to home_prefix(), the name should start with at least six
    characters of garbage (for example, "/ZZZZZZtest.id" in
    test_get_id_no_file()) to prevent stomping on user's files. If the
    file is not being passed to home_prefix(), use the standard library
    function tmpfile() in <stdio.h>

--- tests/check_id.c
+++ tests/check_id.c
@@ -6,9 +6,12 @@
 #include "../src/io.h"
 #include "../src/error.h"
 
-/*
- * Basically just a sanity/SIGSEV test for
- * init_id()
+/**
+ * test_init_id() - a SIGSEV test for test_init_id()
+ *
+ * It's important to make sure that just running init_id()
+ * "as intended" doesn't cause a SIGSEV or other form of
+ * crash behavior.
  */
 START_TEST (test_init_id)
 {
@@ -18,26 +21,21 @@ START_TEST (test_init_id)
 }
 END_TEST
 
-/*
- * When doing stuff with config files, we often need
- * to reset a file to empty. This does that.
+/**
+ * test_get_id_no_file() - test on a non-existant file
+ *
+ * This makes sure that if the file init_id() is called on
+ * doesn't exist that everything doesn't break.
  */
-void reset_file(char *filepath)
+START_TEST (test_get_id_no_file)
 {
-	char x[2*PATH_BUFSIZE];
-	home_prefix(filepath, x);
-	remove(x);
+	/* Setup: make sure file doesn't exist */
+	char y[2*PATH_BUFSIZE];
+	home_prefix("/ZZZZZZtest.id", y);
+	remove(y);
 
-	FILE *fd = fopen(x, "w+");
-	fclose(fd);
-}
-
-/*
- * Do an actual test of  */
-START_TEST (test_get_id)
-{
-	reset_file("/test.id");
-	ck_assert(init_id("/test.id") == 0);
+	/* Make sure there are no initial failures */
+	ck_assert(init_id("/ZZZZZZtest.id") == 0);
 
 	char x[20];
 
@@ -48,24 +46,41 @@ START_TEST (test_get_id)
 	ck_assert_pstr_eq(x, "000002");
 
 	close_id();
+
+	/* Teardown: don't leave random files in the user's home dir */
+	remove(y);
 }
 END_TEST
 
+/**
+ * id_suite() - generate the test suite for id
+ *
+ * id_suite() is a boilerplate function that ties together the above
+ * tests into a single unit.
+ */
 Suite * id_suite(void)
 {
+	/* Boilerplate */
 	Suite *s;
 	TCase *tc_core;
 
 	s = suite_create("ID");
 	tc_core = tcase_create("Core");
 
+	/* Add each test to be run */
 	tcase_add_test(tc_core, test_init_id);
-	tcase_add_test(tc_core, test_get_id);
+	tcase_add_test(tc_core, test_get_id_no_file);
 
 	suite_add_tcase(s, tc_core);
 	return s;
 }
 
+/**
+ * main() - run the test case
+ *
+ * Each module will have a seperate C file that builds an autotools test
+ * using check. main() looks basically identical in any of them.
+ */
 int main (void)
 {
 	int number_failed;

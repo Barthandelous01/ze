ef36a6839ef705068e756e7052ae6a4d07937eae
Caleb D.S. Brzezinski <calebdsb@protonmail.com>
2021-01-03 16:34:38 -0500

    Happy New Year~

--- src/main.c
+++ src/main.c
@@ -13,7 +13,7 @@
 
 static void version()
 {
-	printf("%s", PACKAGE_STRING "\nCopyright (C) 2020 Barthandelous01\n\
+	printf("%s", PACKAGE_STRING "\nCopyright (C) 2020-2021 Barthandelous01\n\
 License RBSD 3-Clause License.\n\
 This is free software; you are free to change and redistribute it.\n\
 There is NO WARRANTY, to the extent permitted by law.\n");
@@ -22,7 +22,7 @@ There is NO WARRANTY, to the extent permitted by law.\n");
 
 static void help()
 {
-	printf("%s\n", PACKAGE_STRING "\nCopyright (C) 2020 Barthandelous01\n"
+	printf("%s\n", PACKAGE_STRING "\nCopyright (C) 2020-2021 Barthandelous01\n"
 		"\n"
 		"OPTIONS:\n"
 		"\t-v, --version      Print version information\n"

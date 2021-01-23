f9cbeea2e5e24e0d30ff86b3a23fa78127b71498
Caleb Brzezinski <cbrzezinski@wccnet.edu>
2021-01-23 18:26:11 -0500

    docs: start conversion to doxygen
    Convert from the current kernel-doc comments to doxygen.
    
    Convert src/conf.c to doxygen comments.

--- src/conf.c
+++ src/conf.c
@@ -13,9 +13,10 @@
 #include "parser.h"
 
 /**
- * append_config() - append a pair to the config struct
- * @cfg: the config * to append to
- * @item: the address of the (presumably heap-allocated) item to append
+ * append a pair to the config struct
+ *
+ * @param[in] cfg: the config * to append to
+ * @param[in] item: the address of the (presumably heap-allocated) item to append
  *
  * Append *item to the *cfg linked list.
  */
@@ -36,8 +37,9 @@ void append_config(config *cfg, config_item *item)
 }
 
 /**
- * free_conf_item() - free a config item recursively
- * @item: the item to recursively free
+ * free a config item recursively
+ *
+ * @param[in] item: the item to recursively free
  *
  * In order to free a linked list, this function uses simple
  * recursion to chain calls to free.
@@ -50,8 +52,9 @@ static void free_conf_item(config_item *item)
 }
 
 /**
- * close_config() - release all the allocated memory
- * @cfg: the config to release
+ * release all the allocated memory
+ *
+ * @param [in] cfg: the config to release
  *
  * close_config() free()s all of the memory allocated in parsing
  * the config file into a singly-linked list.
@@ -64,9 +67,10 @@ int close_config(config *cfg)
 }
 
 /**
- * parse_config() - parse a config file and return the values inside it
- * @pathname: the pathname. Canonicalized; have leading /
- * @cfg: the config struct to fill in
+ * parse a config file and return the values inside it
+ *
+ * @param[in] pathname: the pathname. Canonicalized; have leading /
+ * @param [in] cfg: the config struct to fill in
  *
  * parse_config() is the driver function for the config module. It reads
  * a simple unix-y key-value config file, and returns a singly-linked
@@ -111,10 +115,11 @@ int parse_config(char *pathname, config *cfg)
 }
 
 /**
- * get_config() - get the setting from a config key
- * @cfg: the config header
- * @key: the key to retreive
- * @val: the result buffer
+ * get the setting from a config key
+ *
+ * @param[in] cfg: the config header
+ * @param[in] key: the key to retreive
+ * @param[out] val: the result buffer
  *
  * get_config() returns the value associated with a key and writes it into
  * val. Returns SUCCESS on success, or -EVAL if the key is not found.
